49fdbdbe29cf64199551fd412f95439d475b82ba
Aayush Shrestha <aayushsth77@gmail.com>
2025-07-10 11:07:18 +0545

    test

--- public/pages/footer.php
+++ public/pages/footer.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>footer</title>
 </head>
 <body>
-    <?php require_once './pages/footer.php'; ?>
+    <footer> &copy; <?php echo date("Y"); ?></footer>
 </body>
 </html>
